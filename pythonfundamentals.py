21c13441589e2adad5e0089386c225588b924da2
Darien Chandler <dchandler101@DESKTOP-HOB3O8I.localdomain>
2022-01-20 13:55:30 -0600

    1/20/22

--- pythonfundamentals.py
+++ pythonfundamentals.py
@@ -1,11 +1,33 @@
 #!/usr/bin/python3
-
 print(" Python scripts should start with #!/usr/bin/python3 to be able to run files")
 
-"""
-Multi-line comments 
-
-
-""" 
-
-print("Hello World")
+#Print and Comments 
+#Addition and Subtraction 
+#Multiplication 
+#Division 
+#Integer and Remainder 
+#Variables 
+#Integers 
+#Real Numbers 
+#Complex Numbers 
+#Strings 
+#Referring Elements 
+#Booleans 
+#Combining Conditions 
+#If/Else Expressions 
+#Lists 
+#Tuples 
+#Dictionaries 
+#While Loops
+#For Loop 
+#Min/Max Elements 
+#Absolute Values 
+#Built In Fuctions 
+#Own Functions 
+#Lambda Functions
+#Pandas 
+#Dataframes 
+#Read CSV FIles 
+#Viewing the Data 
+#Data Cleaning 
+#Data Correlations 
