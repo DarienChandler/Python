a3fd0f4992a8addaf3f55474355a43dfa837f8c1
Darien Chandler <dchandler101@DESKTOP-HOB3O8I.localdomain>
2022-01-20 14:05:40 -0600

    1/20/22 Pt.2

--- pythonfundamentals.py
+++ pythonfundamentals.py
@@ -1,33 +1,45 @@
 #!/usr/bin/python3
-print(" Python scripts should start with #!/usr/bin/python3 to be able to run files")
+print("Python scripts should start with #!/usr/bin/python3 to be able to run files")
 
-#Print and Comments 
-#Addition and Subtraction 
-#Multiplication 
-#Division 
-#Integer and Remainder 
-#Variables 
-#Integers 
-#Real Numbers 
-#Complex Numbers 
-#Strings 
-#Referring Elements 
-#Booleans 
-#Combining Conditions 
-#If/Else Expressions 
-#Lists 
-#Tuples 
-#Dictionaries 
-#While Loops
-#For Loop 
-#Min/Max Elements 
-#Absolute Values 
-#Built In Fuctions 
-#Own Functions 
-#Lambda Functions
-#Pandas 
-#Dataframes 
-#Read CSV FIles 
-#Viewing the Data 
-#Data Cleaning 
-#Data Correlations 
+	#Print and Comments 
+
+# Single line comments start with the hashtag symbols 
+"""
+Multi
+Line comments are denoted with  3 quotation marks to start and finish 
+"""
+print("This is how you print text") 
+a = 5 
+print(a)
+
+
+
+	#Addition and Subtraction 
+	#Multiplication 
+	#Division 
+	#Integer and Remainder 
+	#Variables 
+	#Integers 
+	#Real Numbers 
+	#Complex Numbers 
+	#Strings 
+	#Referring Elements 
+	#Booleans 
+	#Combining Conditions 
+	#If/Else Expressions 
+	#Lists 
+	#Tuples 
+	#Dictionaries 
+	#While Loops
+	#For Loop 
+	#Min/Max Elements 
+	#Absolute Values 
+	#Built In Fuctions 
+	#Own Functions 
+	#Lambda Functions
+	#Pandas 
+	#Dataframes 
+	#Read CSV FIles 
+	#Viewing the Data 
+	#Data Cleaning 
+	#Data Correlations 
