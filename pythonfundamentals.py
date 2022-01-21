add2bfdabc6386e963789774555108e3fcf79e75
Darien Chandler <dchandler101@DESKTOP-HOB3O8I.localdomain>
2022-01-20 19:20:11 -0600

    1/20/22 Pt.3

--- pythonfundamentals.py
+++ pythonfundamentals.py
@@ -2,18 +2,6 @@
 print("Python scripts should start with #!/usr/bin/python3 to be able to run files")
 
 	#Print and Comments 
-
-# Single line comments start with the hashtag symbols 
-"""
-Multi
-Line comments are denoted with  3 quotation marks to start and finish 
-"""
-print("This is how you print text") 
-a = 5 
-print(a)
-
-
-
 	#Addition and Subtraction 
 	#Multiplication 
 	#Division 
