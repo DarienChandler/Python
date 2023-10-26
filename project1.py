86604b1090ee45c7d501d2a3bb73df188c4dfeb5
DarienChandler <68358848+DarienChandler@users.noreply.github.com>
2023-10-26 10:31:08 -0500

    Delete project1.py

--- project1.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-
-print("Hello World")
-print("Local Machine to Remote Repo")
-print("Second Commit")
-print("I will upload my projects from my IDE to github using")
-print("This line was made on the local VM on my windows machine")
-print("7/13/23 edit")
-print("10/18/23 edit using Pycharm")
